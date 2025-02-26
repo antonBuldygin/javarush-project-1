5b57d89e1b0ed2d0dc699ba052769358384cfc7a
antonBuldygin <anton.buldygin80@mail.ru>
2025-02-26 21:30:06 +0300

    commit with js script to get info to console from search

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('Happy developing ✨')
+function serializeForm(formNode) {
+    const { elements } = formNode
+
+    const data = Array.from(elements)
+        .filter((item) => !!item.name)
+        .map((element) => {
+            const { name, value } = element
+
+            return { name, value }
+        })
+
+    console.log(data)
+}
+
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    serializeForm(applicantForm)
+}
+
+const applicantForm = document.getElementById('form1')
+applicantForm.addEventListener('submit', handleFormSubmit)
